54bda8b3719ea23f5c0bcedb5336d400ae60acc8
Tohidul29 <tohidswfu@gmail.com>
2022-03-01 01:49:28 +0600

    searching part and card updated

--- js/app.js
+++ js/app.js
@@ -9,20 +9,23 @@ const allMobiles = () =>{
 }
 
 const showSearchingMobiles = (mobiles) => {
-    // console.log(mobiles)
-    for(const mobile of mobiles){
-        const showMobiles = document.getElementById('mobiles-container');
+    console.log(mobiles)
+    const showMobiles = document.getElementById('mobiles-container');
+    showMobiles.textContent = '';
+    mobiles.forEach( mobile => {
         const div = document.createElement('div');
         div.innerHTML = `
-        <div class="col">
-            <div class="card mx-auto text-center">
-                <img class="w-25 mx-auto" src="${mobile.image}" class="card-img-top" alt="...">
+        <div class="col m-4">
+            <div class="card mx-auto text-center p-2">
+                <img class="w-50 mx-auto" src="${mobile.image}" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">${mobile.phone_name}</h5>
+                    <h3 class="card-title">${mobile.phone_name}</h3>
+                    <h5 class="card-title">${mobile.brand}</h5>
+                    <button type="button" class="card-title mt-3 btn col-6 btn-outline-primary">Device Details</button>
                 </div>
             </div>
         </div>
         `;
         showMobiles.appendChild(div);
-    }
+    })
 }
