04dab56c5af0ab870705a1fe063da46204bb8a4d
Tohidul29 <tohidswfu@gmail.com>
2022-03-01 02:26:25 +0600

    device detail api added

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ const allMobiles = () =>{
 }
 
 const showSearchingMobiles = (mobiles) => {
-    console.log(mobiles)
+    // console.log(mobiles)
     const showMobiles = document.getElementById('mobiles-container');
     showMobiles.textContent = '';
     mobiles.forEach( mobile => {
@@ -21,11 +21,31 @@ const showSearchingMobiles = (mobiles) => {
                 <div class="card-body">
                     <h3 class="card-title">${mobile.phone_name}</h3>
                     <h5 class="card-title">${mobile.brand}</h5>
-                    <button type="button" class="card-title mt-3 btn col-6 btn-outline-primary">Device Details</button>
+                    <button onclick="singleDeviceDetails('${mobile.slug}')" type="button" class="card-title mt-3 btn col-6 btn-primary">Device Details</button>
                 </div>
             </div>
         </div>
         `;
         showMobiles.appendChild(div);
     })
+}
+
+const singleDeviceDetails = (mobileSlug) => {
+    // console.log(mobileSlug);
+    const url = `https://openapi.programming-hero.com/api/phone/${mobileSlug}`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => showDeviceDetails(data))
+}
+
+const showDeviceDetails = (deviceInfo) => {
+    const deviceDetailContainer = document.getElementById('device-details');
+    deviceDetailContainer.innerHTML = `
+    <div class="card mb-3 w-50 mx-auto p-4">
+        <img src="${deviceInfo.data.image}" class="card-img-top w-50 mx-auto" alt="...">
+        <div class="card-body text-center">
+            <h5 class="card-title">Card title</h5>
+        </div>
+    </div>
+    `
 }
